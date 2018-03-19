90bce3dc10bda6ae50fdcc44b176451e66c4ff84
WiraXie <wira_xie@ymail.com>
2018-03-19 11:07:07 +0700

    working on routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+
+  bodyClasses = 'skin-blue sidebar-mini';
+  body: HTMLBodyElement = document.getElementsByTagName('body')[0];
+
+  constructor() { }
+
+  ngOnInit() {
+    // add the the body classes
+    this.body.classList.add('skin-blue');
+    this.body.classList.add('sidebar-mini');
+  }
+
+   ngOnDestroy() {
+    // remove the the body classes
+    this.body.classList.remove('skin-blue');
+    this.body.classList.remove('sidebar-mini');
+  }
 }
